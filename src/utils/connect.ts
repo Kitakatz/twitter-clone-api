c72c8e668bc023048113820d7629a03232d86eb1
Andrew Kita <andrewk0792@yahoo.com>
2023-10-29 10:35:01 -0700

    Fixed env file syntax errors.

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -5,7 +5,7 @@ export const mySQLConnection = async (): Promise<Connection> => {
     host     : process.env.MYSQL_HOST,
     user     : process.env.MYSQL_USER,
     password : process.env.MYSQL_PASSWORD,
-    database : process.env.DATABASE
+    database : process.env.MYSQL_DATABASE
   });
 
   return connection;
